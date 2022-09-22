00dc2a14e3293bf433e06e69cb964579bc4f203d
Thiago Sena <lord.sena@gmail.com>
2022-09-22 18:48:21 -0300

    feat(email-service): add kafka to listener reports topic

--- email-service/src/main/java/com/thiagosena/service/email/controllers/EmailController.java
+++ email-service/src/main/java/com/thiagosena/service/email/controllers/EmailController.java
@@ -1,10 +1,12 @@
 package com.thiagosena.service.email.controllers;
 
 import com.thiagosena.email.Email;
+import com.thiagosena.report.Report;
 import com.thiagosena.service.email.exceptions.EmailNotFoundException;
 import com.thiagosena.service.email.repositories.EmailRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,6 +21,12 @@ public class EmailController {
 
     public EmailController(EmailRepository repository) {
         this.repository = repository;
+    }
+
+    @KafkaListener(topics = "reports", groupId = "group")
+    public void listenReports(Report report) {
+        log.info("Got report with id: {}", report.getId());
+        System.out.println("Received Message: " + report);
     }
 
     @GetMapping
